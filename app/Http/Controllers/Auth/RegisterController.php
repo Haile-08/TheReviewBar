91755f0bf72de1708f431057434824f55f6c752b
Haile-08 <haile4cmd@gmail.com>
2026-03-14 15:15:42 +0300

    feat: connect the form with the controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegisterController extends Controller
 {
     public function create()
     {
         return Inertia::render('Auth/Register');
+    }
+
+    public function store(Request $request)
+    {
+        dd($request);
     }
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,150 +1,150 @@
-    <script setup lang="ts">
-    import { toTypedSchema } from "@vee-validate/zod";
-    import { useForm } from "vee-validate";
-    import { z } from "zod";
+<script setup lang="ts">
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import { z } from "zod";
+import { router } from "@inertiajs/vue3";
 
-    import { Button } from "@/components/ui/button";
-    import {
-        FormControl,
-        FormField,
-        FormItem,
-        FormLabel,
-        FormMessage,
-    } from "@/components/ui/form";
-    import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-    const SIZE = 5000000;
-    const TYPES = [
-        "image/jpeg",
-        "image/jpg",
-        "image/png",
-        "image/webp",
-    ];
+const SIZE = 5000000;
+const TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
-    const formSchema = toTypedSchema(
-        z
-            .object({
-                name: z.string().min(2, "Name too short at least 2 characters."),
-                email: z.string().email("Please enter a correct email address."),
-                password: z
-                    .string()
-                    .min(8, "Password too short at least 2 characters."),
-                confirm_password: z.string(),
-                profile_picture: z
-                    .any()
-                    .refine((file) => file instanceof File, "Profile photo is required.")
-                    .refine(
-                        (file) => file?.size <= SIZE,
-                        `Profile photo to to large at least 5MB.`,
-                    )
-                    .refine(
-                        (file) => TYPES.includes(file?.type),
-                        "Profile photo format not supported (.jpg, .jpeg, .png and .webp).",
-                    ),
-            })
-            .refine((data) => data.password === data.confirm_password, {
-                message: "Passwords does not match make sure it matchs.",
-                path: ["confirm_password"],
-            }),
-    );
+const formSchema = toTypedSchema(
+    z
+        .object({
+            name: z.string().min(2, "Name too short at least 2 characters."),
+            email: z.string().email("Please enter a correct email address."),
+            password: z
+                .string()
+                .min(8, "Password too short at least 2 characters."),
+            confirm_password: z.string(),
+            profile_picture: z
+                .any()
+                .refine(
+                    (file) => file instanceof File,
+                    "Profile photo is required.",
+                )
+                .refine(
+                    (file) => file?.size <= SIZE,
+                    `Profile photo to to large at least 5MB.`,
+                )
+                .refine(
+                    (file) => TYPES.includes(file?.type),
+                    "Profile photo format not supported (.jpg, .jpeg, .png and .webp).",
+                ),
+        })
+        .refine((data) => data.password === data.confirm_password, {
+            message: "Passwords does not match make sure it matchs.",
+            path: ["confirm_password"],
+        }),
+);
 
-    const { handleSubmit, setFieldValue } = useForm({
-        validationSchema: formSchema,
-        initialValues: {
-            name: "",
-            email: "",
-            password: "",
-            confirm_password: "",
-        },
-    });
+const { handleSubmit, setFieldValue } = useForm({
+    validationSchema: formSchema,
+    initialValues: {
+        name: "",
+        email: "",
+        password: "",
+        confirm_password: "",
+    },
+});
 
-    const onFileChange = (e: Event) => {
-        const target = e.target as HTMLInputElement;
-        const file = target.files?.[0];
-        setFieldValue("profile_picture", file);
-    };
+const change = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    const file = target.files?.[0];
+    setFieldValue("profile_picture", file);
+};
 
-    const handleSubmitButton = handleSubmit((values) => {
-        console.log(values);
-    });
-    </script>
+const submit = handleSubmit((values) => {
+    console.log(values);
+    router.post("/register", values);
+});
+</script>
 
-    <template>
-        <div class="p-10 w-dvw h-full flex justify-between items-center">
-            <div class="w-[40%] h-full flex justify-center items-center">
-                <form class="w-2/3 space-y-6" @submit="handleSubmitButton">
-                    <FormField name="profile_picture">
-                        <FormItem>
-                            <FormLabel>Profile Picture</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="file"
-                                    accept="image/*"
-                                    @change="onFileChange"
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
+<template>
+    <div class="p-10 w-dvw h-full flex justify-between items-center">
+        <div class="w-[40%] h-full flex justify-center items-center">
+            <form class="w-2/3 space-y-6" @submit="submit">
+                <FormField name="profile_picture">
+                    <FormItem>
+                        <FormLabel>Profile Picture</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="file"
+                                accept="image/*"
+                                @change="change"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
 
-                    <FormField v-slot="{ componentField }" name="name">
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="text"
-                                    placeholder="shadcn"
-                                    v-bind="componentField"
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
+                <FormField v-slot="{ componentField }" name="name">
+                    <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="text"
+                                placeholder="shadcn"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
 
-                    <FormField v-slot="{ componentField }" name="email">
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    v-bind="componentField"
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
+                <FormField v-slot="{ componentField }" name="email">
+                    <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="email"
+                                placeholder="m@example.com"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
 
-                    <FormField v-slot="{ componentField }" name="password">
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="password"
-                                    placeholder="••••••••"
-                                    v-bind="componentField"
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
+                <FormField v-slot="{ componentField }" name="password">
+                    <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="password"
+                                placeholder="••••••••"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
 
-                    <FormField v-slot="{ componentField }" name="confirm_password">
-                        <FormItem>
-                            <FormLabel>Confirm Password</FormLabel>
-                            <FormControl>
-                                <Input
-                                    type="password"
-                                    placeholder="••••••••"
-                                    v-bind="componentField"
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
+                <FormField v-slot="{ componentField }" name="confirm_password">
+                    <FormItem>
+                        <FormLabel>Confirm Password</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="password"
+                                placeholder="••••••••"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
 
-                    <Button type="submit">Register</Button>
-                </form>
-            </div>
+                <Button type="submit">Register</Button>
+            </form>
         </div>
-    </template>
+    </div>
+</template>
