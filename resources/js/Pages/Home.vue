25b6087c5bf70b2ca6ec458dd0b5feb1945d59bd
Haile-08 <haile4cmd@gmail.com>
2026-03-14 13:58:01 +0300

    feat: create the nav link component

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <script setup>
-import { Switch } from "@/components/ui/switch";
-import { Head } from "@inertiajs/vue3"
+import { Head } from "@inertiajs/vue3";
+import { NavLink } from "@/components/shared/NavLink";
 </script>
 
 <template>
-    <Head title="Home"/>
-    <div class="p-10">
-        <Switch default-checked />
-        <h1 class="text-white bg-red-500">Hello</h1>
+    <Head title="Home" />
+    <div class="p-10 w-dvw h-full flex justify-between items-center">
+        <div class="w-full h-full flex justify-center items-center">
+            <NavLink route-name="Login"> Login</NavLink>
+            <NavLink route-name="Register"> Register</NavLink>
+        </div>
     </div>
 </template>
---
+++ resources/js/components/shared/NavLink.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    routeName: {
+        type: String,
+        default: "Home"
+    },
+});
+</script>
+
+<template>
+    <Link :href="route(routeName)" class="">
+        <slot />
+    </Link>
+</template>
