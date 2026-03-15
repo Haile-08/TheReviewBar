5e0374c494215215c41537bedb6f650bd3abb0e0
Haile-08 <haile4cmd@gmail.com>
2026-03-15 12:39:08 +0300

    feat: finish the branch

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Auth\Notifications\VerifyEmail;
-use Illuminate\Auth\Notifications\ResetPassword;
-use App\Models\User;
 
 Route::inertia('/', 'Home')->name('home');
 
